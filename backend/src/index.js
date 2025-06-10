bae362f41bb084a237448d8f7810695740e8689a
nalinpa <122588344+nalinpa@users.noreply.github.com>
2025-06-10 14:35:33 +1200

    FIX: add cors to index

--- backend/src/index.js
+++ backend/src/index.js
@@ -33,7 +33,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(loggingMiddleware);
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:3000" }));
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
